5a9fe30d73862583fc71d64083fad3d7684767f2
srkzz <srkz@LAPTOP-L38B6SM2>
2022-11-29 16:04:23 +0000

    1º Commit - jogo basico de python com pygame para usar as bases de python, desenvolver mais conhecimentos e aprofundar a logistica de pensamento

---
+++ main.py
@@ -0,0 +1,38 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption('Runner')
+clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/PixelType.ttf', 70)
+
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
+text_surface = test_font.render('O MEU JOGO', False, 'Black')
+
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surf.get_rect(midbottom = (600, 300))
+
+player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80,300))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+        # todos os elementos aqui
+        # update a tudo aqui
+
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (300, 50))
+    snail_rect.x -= 4
+    if snail_rect.right <= 0: snail_rect.left = 800
+    screen.blit(snail_surf, snail_rect)
+    player_rect.left += 1
+    screen.blit(player_surf, player_rect)
+    pygame.display.update()
+    clock.tick(60)
